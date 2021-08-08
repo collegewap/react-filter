8e98a01b9c78cd7b719c7eabcc2dbf45a3e9c0c0
Abhishek EH <collegewap@gmail.com>
2021-08-08 13:54:54 +0530

    dropdown search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 let numbers = [64, 84, 22, 32, 83, 65, 51, 26, 23, 56];
 let names = [
@@ -15,9 +15,34 @@ let names = [
   "Dudley",
   "Wood",
 ];
+
+let skills = [
+  "Angular",
+  "CSS",
+  "Graphic Design",
+  "Ember",
+  "HTML",
+  "Information Architecture",
+  "Javascript",
+  "Mechanical Engineering",
+  "Meteor",
+  "NodeJS",
+  "Plumbing",
+  "Python",
+  "Rails",
+  "React",
+  "Kitchen Repair",
+  "Ruby",
+  "UI Design",
+  "User Experience",
+];
 function App() {
   const [filteredNumbers, setFilteredNumbers] = useState(numbers);
   const [searchValue, setSearchValue] = useState("");
+  const [selectedSkill, setSelectedSkill] = useState("");
+  const [dropdownSearchValue, setDropdownSearchValue] = useState("");
+  const [editMode, setEditMode] = useState(false);
+  const dropdownRef = useRef();
 
   const radioChangeHandler = (e) => {
     const value = e.target.value;
@@ -41,6 +66,36 @@ function App() {
       );
     }
   };
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (
+        editMode &&
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target)
+      ) {
+        setEditMode(false);
+      }
+    };
+    document.addEventListener("mousedown", checkIfClickedOutside);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [editMode]);
+
+  const skillSelectionHandler = (skill) => {
+    setSelectedSkill(skill);
+    setDropdownSearchValue("");
+    setEditMode(false);
+  };
+
+  const filteredSkills = skills.filter((skill) =>
+    skill.match(new RegExp(dropdownSearchValue, "i"))
+  );
+
   return (
     <div className="App">
       <h2>Number filtering</h2>
@@ -80,6 +135,43 @@ function App() {
             return <li key={name}>{name} </li>;
           })}
       </ul>
+
+      <h2>Dropdown filtering</h2>
+
+      {editMode ? (
+        <div ref={dropdownRef} className="dropdown-wrapper">
+          <input
+            className="dropdown-input"
+            name="dropdown-input"
+            autoFocus
+            onChange={(e) => setDropdownSearchValue(e.target.value)}
+            value={dropdownSearchValue}
+          />
+          <div className="dropdown-list">
+            <ul>
+              {filteredSkills.map((skill) => {
+                return (
+                  <li key={skill} onClick={() => skillSelectionHandler(skill)}>
+                    {skill}{" "}
+                  </li>
+                );
+              })}
+              {filteredSkills.length === 0 && (
+                <li className="no-result">No results found</li>
+              )}
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <input
+          className={`dropdown-search ${
+            !(dropdownSearchValue || selectedSkill) && "default"
+          }`}
+          onClick={() => setEditMode(true)}
+          onFocus={() => setEditMode(true)}
+          value={selectedSkill || "Select Primary skill"}
+        />
+      )}
     </div>
   );
 }
