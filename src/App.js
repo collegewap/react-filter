5c02779249cb9244dec63e1513a7213dff2d3cd3
Abhishek EH <collegewap@gmail.com>
2021-08-08 11:17:58 +0530

    Number filtering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+let numbers = [64, 84, 22, 32, 83, 65, 51, 26, 23, 56];
 function App() {
+  const [filteredNumbers, setFilteredNumbers] = useState(numbers);
+
+  const radioChangeHandler = (e) => {
+    const value = e.target.value;
+    if (value === "even") {
+      setFilteredNumbers(
+        numbers.filter((number) => {
+          if (number % 2 === 0) {
+            return true;
+          }
+          return false;
+        })
+      );
+    } else {
+      setFilteredNumbers(
+        numbers.filter((number) => {
+          if (number % 2 !== 0) {
+            return true;
+          }
+          return false;
+        })
+      );
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Number filtering</h2>
+      <input
+        type="radio"
+        name="evenOrOdd"
+        id="even"
+        value="even"
+        onChange={radioChangeHandler}
+      />
+      <label htmlFor="even">Even</label>
+      <input
+        type="radio"
+        name="evenOrOdd"
+        id="odd"
+        value="odd"
+        onChange={radioChangeHandler}
+      />
+      <label htmlFor="odd">Odd</label>
+      <ul>
+        {filteredNumbers.map((number) => {
+          return <li key={number}>{number} </li>;
+        })}
+      </ul>
     </div>
   );
 }
